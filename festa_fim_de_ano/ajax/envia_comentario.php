48dd33e495c999294743225325521f3298ad0078
Marcos Lourenco <marcosrealbmata@gmail.com>
2022-05-19 22:21:14 -0300

    listando do banco as opnioes

--- festa_fim_de_ano/ajax/envia_comentario.php
+++ festa_fim_de_ano/ajax/envia_comentario.php
@@ -1,3 +1,18 @@
 <?php
 
-mysql_db_query(string $tb_comentarios, string $query, resource $link_identifier = ?): resource
+
+$result_cliente = "SELECT * FROM tb_comentarios WHERE id_festa='FESTA ANOS 80'  ORDER BY id DESC";
+$resultado_cliente = mysqli_query($link, $result_cliente);
+
+?>
+
+<?php foreach($resultado_cliente as $result){  ?>
+
+
+	
+	<p>Nome: <?php echo $result["autor"]  ?> </p>
+	<p>Data: <?php echo $result["data"]  ?> </p>
+	<p>Comentario: <?php  echo $result["opniao"]  ?> </p>	
+	<p>id festa: <?php echo $result["id_festa"]  ?> </p>
+
+<?php } ?>
